27acb24c4ac9a4ce7b563f4eaafe3acff0d3be93
NithyaNN3 <nithyaniranjani@gmail.com>
2024-05-30 23:53:18 +1000

    removed models from library

--- library/models.py
+++ library/models.py
@@ -1,13 +1,2 @@
-from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    content = models.TextField()
-    date_added = models.FloatField()
-
-    def __str__(self):
-        return self.title
 
