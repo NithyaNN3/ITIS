719d43c0b6de646ce67f8d9b4a20049d355acdff
NithyaNN3 <nithyaniranjani@gmail.com>
2024-05-31 12:02:20 +1000

    added connectivity

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 # users/urls.py
 
 from django.urls import path
-from .views import login
+from .views import login, register, reset_password
 
 urlpatterns = [
-    # path('register/', register, name='register'),
+    path('register/', register, name='register'),
     path('login/', login, name='login'),
+    path('reset-password/', reset_password, name='reset_password')
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def reset_password(request):
+    return render(request, 'reset_password.html')
