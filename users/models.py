cf774b854494385ceaadb43da9bf330aa547e8b2
NithyaNN3 <nithyaniranjani@gmail.com>
2024-05-31 13:03:35 +1000

    annoyyyy

--- users/models.py
+++ users/models.py
@@ -5,5 +5,4 @@ class User(models.Model):
     password = models.CharField(max_length=100)
 
     class Meta:
-        db_table = 'users'  
-        schema = 'users'  
+        db_table = 'users.user' 
