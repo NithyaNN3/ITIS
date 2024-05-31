1f5810a9d053d48ab24432874dec705d16996957
NithyaNN3 <nithyaniranjani@gmail.com>
2024-05-31 10:37:52 +1000

    changed some db  things

--- main/settings.py
+++ main/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
