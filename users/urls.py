a05381e55a24f410a1d4191db133900e01ebba49
NithyaNN3 <nithyaniranjani@gmail.com>
2024-05-31 11:03:21 +1000

    fix

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 # users/urls.py
 
 from django.urls import path
-from .views import register, login_view
+from .views import login_view
 
 urlpatterns = [
     # path('register/', register, name='register'),
