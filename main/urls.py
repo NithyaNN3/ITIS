55831b78858bdc4c808bdcf13eaad61b3cca1d8c
NithyaNN3 <nithyaniranjani@gmail.com>
2024-06-02 20:31:36 +1000

    added namespac

--- main/urls.py
+++ main/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 
+app_name = 'users'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),  
+    path('users/', include(('users.urls', 'users'))),  
 ]
